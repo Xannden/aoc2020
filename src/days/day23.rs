b4525b56072910933d4fc7975c54ec997f3395eb
Xannden <Xannden@gmail.com>
2020-12-24 10:26:25 -0600

    Day 23

--- src/days/day23.rs
+++ src/days/day23.rs
@@ -11,42 +11,128 @@ pub fn run() {
 
     // println!("{:?}", data);
 
-    part1(data.clone());
-    part2();
+    part1(&data);
+    part2(&data);
 }
 
-fn parse(input: String) -> Vec<u8> {
-    input.as_bytes().iter().map(|b| *b - b'0').collect()
+fn parse(input: String) -> Vec<usize> {
+    input
+        .as_bytes()
+        .iter()
+        .map(|b| (*b - b'0') as usize)
+        .collect()
 }
 
-#[allow(clippy::needless_collect)]
-fn part1(mut input: Vec<u8>) {
+fn part1(input: &[usize]) {
     println!("Part 1");
 
-    let min = *input.iter().min().unwrap();
     let max = *input.iter().max().unwrap();
+    let mut state = [input[0]; 10];
 
-    for turn in 0..10 {
-        println!("-- move {} --", turn + 1);
-        println!("current cup: {}", input[turn % input.len()]);
-        println!("cups: {:?}", input);
-
-
-        // println!("pickup: {:?}", cups);
-
-        // let mut dest = if cup == min { max } else { cup - 1 };
-
-
-        // println!("destination: {}", dest);
-
-
-
-        println!();
+    for val in input.windows(2) {
+        state[val[0]] = val[1];
     }
 
-    println!("{:?}", input);
+    let mut current = input[0];
+    for turn in 0..100 {
+        // println!("-- move {} --", turn + 1);
+        // print!("cups: ({})", current);
+
+        // let mut temp = state[current];
+        // while temp != current {
+        //     print!(" {}", temp);
+        //     temp = state[temp];
+        // }
+        // println!();
+
+        //Pick up the next three
+        let mut temp = current;
+        let mut pickup = Vec::new();
+        for _ in 0..3 {
+            pickup.push(state[temp]);
+            temp = state[temp];
+        }
+        state[current] = state[temp];
+        // println!("pick up: {:?}", pickup);
+
+        let mut dest = if current == 1 { max } else { current - 1 };
+        while pickup.contains(&dest) {
+            dest = if dest == 1 { max } else { dest - 1 };
+        }
+
+        // println!("destination: {}", dest);
+        state[pickup[2]] = state[dest];
+        state[dest] = pickup[0];
+
+        // println!("{:?}", state);
+
+        current = state[current];
+    }
+
+    let mut pos = state[1];
+    let mut result = String::new();
+    while pos != 1 {
+        result.push_str(&pos.to_string());
+        pos = state[pos];
+    }
+
+    println!("Result {}", result);
 }
 
-fn part2() {
+fn part2(input: &[usize]) {
     println!("Part 2");
+
+    let max = 1000000;
+    let mut state = Vec::with_capacity(1000000);
+
+    for idx in 1..=1000000 {
+        state.push(idx);
+    }
+
+    for val in input.windows(2) {
+        state[val[0]] = val[1];
+    }
+
+    state[*input.last().unwrap()] = 10;
+    state.push(input[0]);
+
+    // println!("{:?}", state);
+
+    let mut current = input[0];
+    for turn in 0..10000000 {
+        // println!("-- move {} --", turn + 1);
+        // print!("cups: ({})", current);
+
+        // let mut temp = state[current];
+        // while temp != current {
+        //     print!(" {}", temp);
+        //     temp = state[temp];
+        // }
+        // println!();
+
+        //Pick up the next three
+        let mut temp = current;
+        let mut pickup = Vec::new();
+        for _ in 0..3 {
+            pickup.push(state[temp]);
+            temp = state[temp];
+        }
+        state[current] = state[temp];
+        // println!("pick up: {:?}", pickup);
+
+        let mut dest = if current == 1 { max } else { current - 1 };
+        while pickup.contains(&dest) {
+            dest = if dest == 1 { max } else { dest - 1 };
+        }
+
+        // println!("destination: {}", dest);
+        state[pickup[2]] = state[dest];
+        state[dest] = pickup[0];
+
+        // println!("{:?}", state);
+
+        current = state[current];
+    }
+
+    println!("Result {}", state[state[state[0]]] * state[state[0]]);
 }
